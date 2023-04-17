a4faf4b24bab574be40dbba3a947b6186453cf06
Second-Son-of-Mozzarella <Mohtzee@protonmail.com>
2023-04-17 14:11:52 -0500

    connected to the database

---
+++ Model/Territory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EJS_DOTNET_M12_D1.Model
+{
+    public partial class Territory
+    {
+        public Territory()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public string TerritoryId { get; set; }
+        public string TerritoryDescription { get; set; }
+        public int RegionId { get; set; }
+
+        public virtual Region Region { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
