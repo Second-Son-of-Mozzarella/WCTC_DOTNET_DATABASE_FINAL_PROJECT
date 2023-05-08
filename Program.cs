c8cbd3308cb702c9207de0f8b6b5d5e241c6f503
Second-Son-of-Mozzarella <Mohtzee@protonmail.com>
2023-05-08 14:45:34 -0500

    End of Week 7

--- Program.cs
+++ Program.cs
@@ -173,7 +173,12 @@ try
                                 Console.ForegroundColor = ConsoleColor.White;
                                 foreach (Product p in item.Products)
                                 {
-                                    Console.WriteLine($"\t{p.ProductName}");
+                                    if(p.Discontinued == true)
+                                    {
+                                        Console.WriteLine($"\t{p.ProductName} - Discontinued");
+                                    }
+                                    else{Console.WriteLine($"\t{p.ProductName}");}
+                                    
                                 }
                                 Console.WriteLine();
 
@@ -215,7 +220,7 @@ try
                                 Console.ForegroundColor = ConsoleColor.White;
                                 foreach (Product p in item.Products.Where(p => p.Discontinued == true))
                                 {
-                                    Console.WriteLine($"\t{p.ProductName}");
+                                    Console.WriteLine($"\t{p.ProductName} \tDiscontinued {p.Discontinued}");
                                 }
                                 Console.WriteLine();
 
@@ -254,6 +259,7 @@ try
                         Console.ForegroundColor = ConsoleColor.White;
                         foreach (Product p in item.Products)
                         {
+
                             Console.WriteLine($"[{p.ProductId}] {p.ProductName}");
                         }
                         Console.WriteLine();
@@ -309,102 +315,6 @@ try
 
 
 
-
-
-        // Console.WriteLine("1) Display Categories");
-        // Console.WriteLine("2) Add Category");
-        // Console.WriteLine("3) Display Category and related products");
-        // Console.WriteLine("4) Display all Categories and their related products");
-        // Console.WriteLine("\"q\" to quit");
-
-        // if (choice == "1")
-        // {
-        //     var query = db.Categories.OrderBy(p => p.CategoryName);
-
-        //     Console.ForegroundColor = ConsoleColor.Green;
-        //     Console.WriteLine($"{query.Count()} records returned");
-        //     Console.ForegroundColor = ConsoleColor.Magenta;
-        //     foreach (var item in query)
-        //     {
-        //         Console.WriteLine($"{item.CategoryName} - {item.Description}");
-        //     }
-        //     Console.ForegroundColor = ConsoleColor.White;
-
-        //     keyToContinue();
-        // }
-
-        // else if (choice == "2")
-        // {
-        //     Category category = new Category();
-        //     Console.WriteLine("Enter Category Name:");
-        //     category.CategoryName = Console.ReadLine();
-        //     Console.WriteLine("Enter the Category Description:");
-        //     category.Description = Console.ReadLine();
-        //     ValidationContext context = new ValidationContext(category, null, null);
-        //     List<ValidationResult> results = new List<ValidationResult>();
-
-        //     var isValid = Validator.TryValidateObject(category, context, results, true);
-        //     if (isValid)
-        //     {
-        //         // logger.Info("Validation passed");
-        //         // check for unique name
-        //         if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
-        //         {
-        //             // generate validation error
-        //             isValid = false;
-        //             results.Add(new ValidationResult("Name exists", new string[] { "CategoryName" }));
-        //         }
-        //         else
-        //         {
-        //             logger.Info("Validation passed");
-        //             // TODO: save category to db
-        //         }
-        //     }
-        //     if (!isValid)
-        //     {
-        //         foreach (var result in results)
-        //         {
-        //             logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
-        //         }
-        //     }
-        // }
-        // else if (choice == "3")
-        // {
-        //     var query = db.Categories.OrderBy(p => p.CategoryId);
-
-        //     Console.WriteLine("Select the category whose products you want to display:");
-        //     Console.ForegroundColor = ConsoleColor.DarkRed;
-        //     foreach (var item in query)
-        //     {
-        //         Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
-        //     }
-        //     Console.ForegroundColor = ConsoleColor.White;
-        //     int id = int.Parse(Console.ReadLine());
-        //     Console.Clear();
-        //     logger.Info($"CategoryId {id} selected");
-        //     Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
-        //     Console.WriteLine($"{category.CategoryName} - {category.Description}");
-        //     foreach (Product p in category.Products)
-        //     {
-        //         Console.WriteLine($"\t{p.ProductName}");
-        //     }
-
-        //     keyToContinue();
-        // }
-        // else if (choice == "4")
-        // {
-        //     var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
-        //     foreach (var item in query)
-        //     {
-        //         Console.WriteLine($"{item.CategoryName}");
-        //         foreach (Product p in item.Products)
-        //         {
-        //             Console.WriteLine($"\t{p.ProductName}");
-        //         }
-        //     }
-        //     keyToContinue();
-        // }
-        // Console.WriteLine();
 
     } while (choice.ToLower() != "0");
 }
